78d82cc2ca74e4380c8c9043bcad28bbe9253fec
Kim9 <jgh19911212@yahoo.co.jp>
2017-08-15 00:07:48 +0900

    coredata manager class

--- talk/Model/CoreData/LoginHistory+CoreDataProperties.swift
+++ talk/Model/CoreData/LoginHistory+CoreDataProperties.swift
@@ -16,8 +16,31 @@ extension LoginHistory {
         return NSFetchRequest<LoginHistory>(entityName: "LoginHistory")
     }
 
-    @NSManaged public var id: String?
+    @NSManaged public var userId: String?
     @NSManaged public var password: String?
     @NSManaged public var updateDate: NSDate?
-
+    
+    class func createNewRecord() -> LoginHistory {
+        let tweet = NSEntityDescription.entity(forEntityName: EntityName.LoginHistory.rawValue, in: CoreDataManager.shared.managedObjectContext)
+        let history = LoginHistory(entity: tweet!, insertInto: CoreDataManager.shared.managedObjectContext)
+        return history
+    }
+    
+    class func readLastLoginHistory() -> LoginHistory? {
+        
+        var lastHistory: LoginHistory? = nil
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>()
+//        let predicate = NSPredicate(format: "%K = %s", "userId", "tt")
+//        fetchRequest.predicate = predicate
+        do {
+            let fetchData = try CoreDataManager.shared.managedObjectContext.fetch(fetchRequest)
+            for data in fetchData {
+                lastHistory = data as? LoginHistory
+            }
+        } catch {
+        
+        }
+        return lastHistory
+    }
 }
