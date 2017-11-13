baf44c874c5731ce60c1d361ad438ed000bba19f
Kim9 <jgh19911212@yahoo.co.jp>
2017-11-13 22:10:58 +0900

    1113

--- talk/Resource/Common.swift
+++ talk/Resource/Common.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class Common: NSObject {
+    
+    // MARK: - Date <-> String
 
     class func dateToString(date: Date?, format: String) -> String? {
         
@@ -25,6 +27,23 @@ class Common: NSObject {
             let formatter = DateFormatter()
             formatter.dateFormat = format
             return formatter.date(from: guradDateString)
+        }
+        return nil
+    }
+    
+    // MARK: - UIImage <-> String
+
+    class func imageToString(image: UIImage?) -> String? {
+        if let guardImage = image {
+            let pngData = UIImagePNGRepresentation(guardImage)
+            return pngData?.base64EncodedString(options: .endLineWithCarriageReturn)
+        }
+        return nil
+    }
+    
+    func stringToImage(base64String: String?) -> UIImage? {
+        if let guardBase64String = base64String, let pngData = Data(base64Encoded: guardBase64String, options: .ignoreUnknownCharacters) {
+            return UIImage(data: pngData)
         }
         return nil
     }
