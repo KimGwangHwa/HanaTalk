a5cf2f4699579b9d1c6d19966be2ae6734d6a7f8
kim.mac <kim.gh171212@gmail.com>
2018-01-05 02:41:29 +0900

    0105

--- talk/View/Album/AlbumViewController.swift
+++ talk/View/Album/AlbumViewController.swift
@@ -9,19 +9,19 @@ import UIKit
 import AVFoundation
 import Photos
 
-class AlbumViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class AlbumViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIViewControllerPreviewingDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
     var dataSource = [UIImage]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        collectionView.register(R.nib.albumCell(), forCellWithReuseIdentifier: R.reuseIdentifier.albumCell.identifier)
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 100, height: 100)
-        collectionView.collectionViewLayout = layout
+        
+        setUpView()
         
         let options = PHFetchOptions()
+        options.sortDescriptors = [
+            NSSortDescriptor(key: "creationDate", ascending: false)
+        ]
         let assets: PHFetchResult = PHAsset.fetchAssets(with: .image, options: options)
         assets.enumerateObjects { (asset, index, stop) in
             let manager: PHImageManager = PHImageManager()
@@ -32,6 +32,22 @@ class AlbumViewController: UIViewController, UICollectionViewDelegate, UICollect
                 }
             }
         }
+    }
+    
+    func setUpView() {
+        registerForPreviewing(with: self, sourceView: view)
+
+        collectionView.register(R.nib.albumCell(), forCellWithReuseIdentifier: R.reuseIdentifier.albumCell.identifier)
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 2.5
+        let rowCount: CGFloat = 3
+        let itemWidth = (UIScreen.main.bounds.size.width - (spacing * (rowCount - 1 ))) / rowCount
+        
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        collectionView.collectionViewLayout = layout
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,5 +68,35 @@ class AlbumViewController: UIViewController, UICollectionViewDelegate, UICollect
         }
         return UICollectionViewCell()
     }
-
+    
 }
+
+// MARK: 3D Touch
+extension AlbumViewController {
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        let peekPreView = R.storyboard.peek.peekViewController()
+        
+        if let indexPath = collectionView.indexPathForItem(at: location) {
+            peekPreView?.image = dataSource[indexPath.row]
+        }
+        return peekPreView
+    }
+
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+//        let viewcontroller = UIViewController()
+//        viewcontroller.view.backgroundColor = UIColor.blue
+//
+//        show(viewcontroller, sender: nil)
+    }
+
+//    override var previewActionItems: [UIPreviewActionItem] {
+//
+//        let action1 = UIPreviewAction(title: "Select", style: .default) { (action, viewController) in
+//
+//        }
+//
+//        return [action1]
+//    }
+}
+
---
+++ talk/View/Peek/PeekViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PeekViewController.swift
+//  talk
+//
+//  Created by ひかりちゃん on 2018/01/03.
+//
+
+import UIKit
+
+class PeekViewController: UIViewController {
+    
+    public var image: UIImage?
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = image
+        // Do any additional setup after loading the view.
+    }
+    
+//    override var previewActionItems: [UIPreviewActionItem] {
+//        let action =  UIPreviewAction(title: "test", style: .default) { (action, viewController) in
+//            
+//        }
+//        return [action]
+//    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
