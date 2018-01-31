45ce7b4901d97015efcf0c692ffab25a9f6e6da4
kin <ari-kouka_kin@jmas.co.jp>
2018-01-31 17:41:08 +0900

    0131 2

--- talk/View/SignUp/SignUpViewController.swift
+++ talk/View/SignUp/SignUpViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 import Parse
 
 class SignUpViewController: UITableViewController {
-
-    @IBOutlet weak var userIdTextField: UITextField!
+    
+    var isMan = true
+    @IBOutlet weak var nickNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var confirmTextField: UITextField!
+    @IBOutlet weak var womenButton: UIButton!
+    @IBOutlet weak var menButton: UIButton!
+    @IBOutlet weak var eMailTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +24,35 @@ class SignUpViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func sexButtonEvent(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+
+        if sender == womenButton && sender.isSelected {
+            isMan = false
+        } else {
+            isMan = true
+        }
+    }
+    
     @IBAction func submitEvent(_ sender: Any) {
         let signupUser = User()
-        signupUser.userName = userIdTextField.text ?? ""
+        signupUser.userName = eMailTextField.text ?? ""
         signupUser.password = passwordTextField.text ?? ""
 
         UserApi.signUp(user: signupUser) { (status) in
             if status == .success {
-                if let viewController = R.storyboard.addUserInfo.addUserInfoViewController() {
-                        self.navigationController?.pushViewController(viewController, animated: true)
-                }
+                
+                let userinfo = UserInfo()
+                userinfo.email = self.eMailTextField.text
+                userinfo.sex = self.isMan
+                userinfo.nickName = self.nickNameTextField.text
+                UserInfoApi.saveUserInfo(userInfo: userinfo, completion: { (state) in
+                    if state == .success {
+                        if let viewController = R.storyboard.home.homeViewController() {
+                            self.navigationController?.pushViewController(viewController, animated: true)
+                        }
+                    }
+                })
             }
         }
     }
