cc176ca28be6f2f3c1d022ae1537c2203734c6ec
kim.mac <kim.gh171212@gmail.com>
2018-03-22 22:43:52 +0900

    0322-2

--- talk/View/TalkRoom/TalkRoomViewController.swift
+++ talk/View/TalkRoom/TalkRoomViewController.swift
@@ -8,20 +8,17 @@
 
 import UIKit
 
-class TalkRoomViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, DidReceiveMessageDelegate {
+class TalkRoomViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var inputTextField: UITextField!
     
     private var receiverUserInfo: UserInfo? = nil
-    
-    public var receiverUserId: String = ""
-    
     private var chatRoom: ChatRoom! = nil
     
     private var dataSource = [Message]()
-    
+
     override func viewWillAppear(_ animated: Bool) {
         self.view.layoutIfNeeded()
 
@@ -29,32 +26,8 @@ class TalkRoomViewController: UIViewController, UITableViewDelegate, UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addKeyBoardObserver()
+        addObserver()
         setUpView()
-        readData()
-    }
-    
-    func readData() {
-        UserInfo.findUserInfo(byObjectId: receiverUserId) { (userInfo, isSuccess) in
-            self.receiverUserInfo = userInfo
-            self.enterTheChatRoom()
-        }
-    }
-    
-    func enterTheChatRoom() {
-        
-        RemoteChatManager.shared.addDelegate(self)
-        
-        RemoteChatManager.shared.enterTheChatRoom(userName: receiverUserId) { (isSuccess, chatRoom) in
-            if isSuccess == true {
-                self.chatRoom = chatRoom
-
-//                if let messages = MessageDao.find(chatName: self.chatRoom.name ?? "") {
-//                    self.dataSource.append(contentsOf: messages)
-//                    self.tableView.reloadData()
-//                }
-            }
-        }
     }
     
     func setUpView() {
@@ -74,23 +47,76 @@ class TalkRoomViewController: UIViewController, UITableViewDelegate, UITableView
         }
     }
     
-    // MARK: - KeyBoardObserver
     
-    func addKeyBoardObserver() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    
+    // MARK: - TapEvent
+    
+    @IBAction func tapTableViewEvent(_ sender: Any) {
+        view.endEditing(false)
+    }
+
+    @IBAction func sendEvent(_ sender: Any) {
+        
     }
     
-    func removeKeyBoardObserver() {
+    deinit {
         NotificationCenter.default.removeObserver(self)
     }
+
+}
+
+// MARK: - UITabelViewDelegate
+extension TalkRoomViewController: UITableViewDelegate, UITableViewDataSource {
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let rowData = dataSource[indexPath.row]
+        /*
+         
+         if rowData.responderState == .Receive {
+         if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.receiveTextCell.identifier, for: indexPath) as? ReceiveTextCell {
+         cell.message = dataSource[indexPath.row]
+         cell.receiver = receiverUserInfo
+         return cell
+         }
+         
+         } else if rowData.responderState == .Send {
+         if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.sendTextCell.identifier, for: indexPath) as? SendTextCell {
+         cell.message = dataSource[indexPath.row]
+         return cell
+         }
+         }
+         */
+        return UITableViewCell()
+    }
+}
+
+// MARK: - NotificationCenter
+
+extension TalkRoomViewController {
+    
+    func addObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .PushNotificationDidRecive, object: nil)
+    }
+    
+    
+
+    @objc func pushNotificationDidReceive(notification: Notification?) {
+        
+    }
+
     @objc func keyboardWillShow(notification: Notification?) {
         let rect = (notification?.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
         let duration: TimeInterval? = notification?.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? Double
         let height = rect?.size.height
         self.bottomConstraint.constant = height ?? 0.0
-
+        
         UIView.animate(withDuration: duration!, animations: { () in
             self.view.layoutIfNeeded()
         })
@@ -103,61 +129,6 @@ class TalkRoomViewController: UIViewController, UITableViewDelegate, UITableView
             self.view.layoutIfNeeded()
         })
     }
-    
-    // MARK: - TapEvent
-    
-    @IBAction func tapTableViewEvent(_ sender: Any) {
-        view.endEditing(false)
-    }
 
-    @IBAction func sendEvent(_ sender: Any) {
-        
-       RemoteChatManager.shared.sendTextMessage(inputTextField.text ?? "", receiver: receiverUserId, chatRoom: chatRoom) { (isSuccess, message) in
-            if let guardMessage = message {
-                self.dataSource.append(guardMessage)
-                self.tableView.reloadData()
-            
-                self.inputTextField.resignFirstResponder()
-                self.inputTextField.text = nil
-            }
-        }
-    }
-
-    // MARK: - UITabelViewDelegate
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let rowData = dataSource[indexPath.row]
-        /*
- 
-        if rowData.responderState == .Receive {
-            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.receiveTextCell.identifier, for: indexPath) as? ReceiveTextCell {
-                cell.message = dataSource[indexPath.row]
-                cell.receiver = receiverUserInfo
-                return cell
-            }
-            
-        } else if rowData.responderState == .Send {
-            if let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.sendTextCell.identifier, for: indexPath) as? SendTextCell {
-                cell.message = dataSource[indexPath.row]
-                return cell
-            }
-        }
-        */
-        return UITableViewCell()
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
